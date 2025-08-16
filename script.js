7f276d73c68b7b00019d031aa5e7c9ab3e50f000
monje122 <166882577+monje122@users.noreply.github.com>
2025-08-16 12:26:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -116,11 +116,18 @@ async function mostrarVentana(id) {
   return;
 }
   }
- if (id === 'pago') {
-    
-     if (modoCartones === 'fijo' && usuario.cartones.length !== cantidadFijaCartones) {
-    return alert(`Debes elegir exactamente ${cantidadFijaCartones} cartones antes de continuar.`);
-}
+if (id === 'pago') {
+    if (modoCartones === 'fijo') {
+      if (usuario.cartones.length !== cantidadFijaCartones) {
+        alert(`Debes elegir exactamente ${cantidadFijaCartones} cartones antes de continuar.`);
+        return;
+      }
+    } else { // modo libre
+      if (usuario.cartones.length !== cantidadPermitida) {
+        alert(`Debes elegir exactamente ${cantidadPermitida} cartones antes de continuar.`);
+        return;
+      }
+    }
   }
   // Ahora mostramos la ventana deseada
   document.querySelectorAll('section').forEach(s => s.classList.add('oculto'));
